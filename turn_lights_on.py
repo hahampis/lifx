fee066ff86415a60c51c26795d9a3304c11dda10
Nikolaos Chachampis <nikos_chachampis@ownmail.net>
2017-06-05 21:15:42 +0300

    Don't turn the lights on during day time.

--- turn_lights_on.py
+++ turn_lights_on.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 
 import requests
+from datetime import datetime
+from astral import Astral
 
-token = open('/home/osmc/token').read().strip()
+def turn_lights_on():
+    token = open('/home/osmc/token').read().strip()
 
-data = {"power": "on", "color": "kelvin:2700 brightness:1.0"}
-response = requests.put('https://api.lifx.com/v1/lights/all/state',
-                         data=data, auth=(token, ''))
+    data = {"power": "on", "color": "kelvin:2700 brightness:1.0"}
+    response = requests.put('https://api.lifx.com/v1/lights/all/state',
+                             data=data, auth=(token, ''))
+
+def get_sunlight_info():
+    a = Astral()
+    city = a['Athens']
+    return city.sun()
+
+if __name__ == '__main__':
+    sunlight_info = get_sunlight_info()
+    sunrise = sunlight_info['sunrise']
+    sunset = sunlight_info['sunset']
+    now = datetime.now(tz=sunrise.tzinfo)
+    if now > sunset or now < sunrise:
+        turn_lights_on()
